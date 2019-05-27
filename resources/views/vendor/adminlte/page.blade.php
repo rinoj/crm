8bd9a43833c76cd89cfeed9d2eb25c99bcfb622f
rinoj <jeezybox@gmail.com>
2019-05-27 19:22:47 +0200

    Excel
    Excel package installed

--- resources/views/vendor/adminlte/page.blade.php
+++ resources/views/vendor/adminlte/page.blade.php
@@ -60,6 +60,12 @@
 
                     <ul class="nav navbar-nav">
                         <li>
+                            <a href="#">
+                                <i class="fa fa-fw fa-calendar"></i> Appointments
+                            </a>
+                           
+                        </li>
+                        <li>
                             @if(config('adminlte.logout_method') == 'GET' || !config('adminlte.logout_method') && version_compare(\Illuminate\Foundation\Application::VERSION, '5.3.0', '<'))
                                 <a href="{{ url(config('adminlte.logout_url', 'auth/logout')) }}">
                                     <i class="fa fa-fw fa-power-off"></i> {{ trans('adminlte::adminlte.log_out') }}
